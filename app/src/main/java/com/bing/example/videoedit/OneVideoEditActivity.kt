43443b557ef8cfe4e4125e0947963e28c6e33906
beiger <1441512921@qq.com>
2019-01-14 22:15:12 +0800

    video edit

--- app/src/main/java/com/bing/example/videoedit/OneVideoEditActivity.kt
+++ app/src/main/java/com/bing/example/videoedit/OneVideoEditActivity.kt
@@ -4,7 +4,8 @@ import androidx.lifecycle.ViewModelProviders
 import com.bing.example.R
 import com.bing.mvvmbase.base.BaseActivity
 import com.bing.example.databinding.ActivityOneVideoEditBinding
-import com.bing.mvvmbase.base.BaseViewModel
+import com.github.clans.fab.FloatingActionButton
+import com.github.clans.fab.FloatingActionButton.SIZE_NORMAL
 
 class OneVideoEditActivity : BaseActivity<ActivityOneVideoEditBinding, OneVideoEditViewModel>() {
 
@@ -15,11 +16,30 @@ class OneVideoEditActivity : BaseActivity<ActivityOneVideoEditBinding, OneVideoE
         override fun initViewModel() {
                 mViewModel = ViewModelProviders.of(this).get(OneVideoEditViewModel::class.java)
                 val videoPath = intent.getStringExtra("videopath")
-                mViewModel.editInfo = EditInfo(videoPath)
+                mViewModel.editInfo = EditInfo(videoPath?: "")
         }
 
         override fun bindAndObserve() {
-                mBinding.editInfo = mViewModel.editInfo
+                mBinding.editInfo = mViewModel.editInfotFloatingMenu()
         }
 
+        fun initFloatingMenu() {
+                val button0 = FloatingActionButton(this)
+                button0.buttonSize = SIZE_NORMAL
+                button0.labelText = "hahaha"
+                mBinding.menu.addMenuButton(button0, 0)
+
+                val button1 = FloatingActionButton(this)
+                button1.buttonSize = SIZE_NORMAL
+                button0.labelText = "hahaha"
+                mBinding.menu.addMenuButton(button1, 1)
+
+
+                val button2 = FloatingActionButton(this)
+                button2.buttonSize = SIZE_NORMAL
+                button0.labelText = "hahaha"
+                mBinding.menu.addMenuButton(button2, 2)
+
+                mBinding.menu.setClosedOnTouchOutside(true)
+        }
 }
--- app/src/main/java/com/bing/example/videoedit/OneVideoEditViewModel.kt
+++ app/src/main/java/com/bing/example/videoedit/OneVideoEditViewModel.kt
@@ -1,8 +1,9 @@
 package com.bing.example.videoedit
 
+import android.app.Application
 import com.bing.example.app.ScreenRecordApplication
 import com.bing.mvvmbase.base.BaseViewModel
 
-class OneVideoEditViewModel(application: ScreenRecordApplication): BaseViewModel(application) {
+class OneVideoEditViewModel(application: Application): BaseViewModel(application) {
         lateinit var editInfo: EditInfo
 }
